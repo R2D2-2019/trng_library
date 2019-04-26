4aed992801a5fcd7cb15623f7453ea263bebb327
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-04-26 09:54:48 +0200

    added documentation to the main

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,14 @@
 #include <trng.hpp>
 
 int main(){
+    // init the trng
     r2d2::trng_c::init();
 
+    // wait until we have a new random number
     while(!r2d2::trng_c::value_available()){∏
 
-    r2d2::trng_c::get();
+    hwlib::cout << "Random number: " << r2d2::trng_c::get() << '\n';
+
+    // infinite loop
+    while(true){}
 }

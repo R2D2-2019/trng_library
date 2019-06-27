87596fb5ed25869ac3c05209878eaf06f1feca53
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-06-27 14:17:28 +0200

    bugfix not changing the clock speed of the sam3x8e

--- code/main.cpp
+++ code/main.cpp
@@ -4,6 +4,9 @@ int main(){
     // init the trng
     r2d2::trng_c::init();
 
+    // init timers arduino
+    hwlib::wait_ms(1);
+
     // wait until we have a new random number
     while(!r2d2::trng_c::value_available()){}
 
